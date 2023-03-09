6dae9a07837979fcade3c882ef16e776c28a70f8
Niharika Kuchhal <niharikakuchhal4@gmail.com>
2023-03-09 21:52:25 +0530

    added patter2.py file to our project

--- pattern2.py
+++ pattern2.py
@@ -1,4 +1,7 @@
-print("printing the pattern')
-for i in range(int(input('Enter the size:'))):
-	print('*'*i)
+"""Printing the right angle"""
+
+print("printing the pattern")
+
+for i in range(1,int(input('Enter the size:'))+1):
+	print(('*'*i).rjust(10,))
 
