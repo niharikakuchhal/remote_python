0ecf5d09d69937855002428e1cd4e2924f25965e
Niharika Kuchhal <niharikakuchhal4@gmail.com>
2023-03-09 18:39:48 +0530

    Added a file pattern.py to add addition features to our project

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a README file,that describe the project.
+In this differnt type of pattern is printing.
+
+~README.md - describe the file
+~pattern.py - print the pattern
+
--- pattern.py
+++ pattern.py
@@ -1,5 +1,11 @@
+"""pattern.py simple pattern printing program"""
+
 print('Pattern Printing')
+
 num = int(input('Enter the size of pattern:'))
-for _ in range(num):
+
+for _ in range(1,num+1):
 	print('*'*_)
+	
+print('End of the program')
 
